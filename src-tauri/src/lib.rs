19b406a43a7e050dc8d3b880d58f7f5cc042c35c
Fal-hi <garethbalefal@gmail.com>
2026-06-22 11:02:43 +0700

    add tauri config release

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -73,6 +73,7 @@ pub fn run() {
     ];
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations("sqlite:daily_spend.db", migrations)
